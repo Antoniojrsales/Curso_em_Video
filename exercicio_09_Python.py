c53c0ea4b25d7a3e55e7ffacabb0a2a9074a70c4
Antonio Gomes Sales junior <Antoniogomes.junio@gmail.com>
2024-10-10 20:10:22 -0300

    Refatorando o script com melhorias

--- exercicio_09_Python.py
+++ exercicio_09_Python.py
@@ -11,3 +11,42 @@ print(f'{numero} x  8 = {numero * 8}')
 print(f'{numero} x  9 = {numero * 9}')
 print(f'{numero} x 10 = {numero * 10}')
 print('-'*11)
+
+
+'''
+# Obtendo um script mais estruturado
+
+"""Função que exibe a tabuada de um número."""
+def tabuada(num):
+    for i in range(1, 11): # Laço de repetição que vai de 1 a 10
+        print(f'{num} x  {i} = {num * i}') #Msg do resultado da tabuada
+
+"""Funcao que obtém e valida a entrada do usuário."""
+def obter_dados():
+    while True:
+        numero = input('Digite [s/S] para finalizar o programa ou um valor valido para tabuada: ').strip().upper() # Entrada do usuario
+
+        if numero == 'S': # Finaliza o programa se o usuário digitar 'S'
+            print('Muito obrigado por usar nosso programa.')
+            print('Programa finalizado com sucesso.')
+            return None
+        
+        try:
+            numero_int = int(numero) # Tenta converter a entrada para um inteiro
+            return numero_int
+        except ValueError:
+            print('Erro: você não digitou um valor válido. Tente novamente.') # Msg de erro caso algo de errado
+
+"""Função principal que executa o programa de Tabuada."""
+def main():
+     while True:
+        numero = obter_dados() # Obtém o número do usuário
+
+        if numero is None:
+             break # Sai do loop se o usuário escolher finalizar
+        
+        tabuada(numero) # Exibe a tabuada do número 
+
+# Inicia o programa
+if __name__ == "__main__":
+    main()'''
